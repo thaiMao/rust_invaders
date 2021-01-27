79403bbc613e844972f6f621dfe1153d91d0be20
thai <maot.tm@gmail.com>
2021-01-27 22:40:45 +0000

    player module

---
+++ src/player.rs
@@ -0,0 +1,27 @@
+use crate::{ NUM_COLS, NUM_ROWS };
+
+pub struct Player {
+    x: usize,
+    y: usize,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            x: NUM_COLS / 2,
+            y: NUM_ROWS - 1
+        }
+    }
+
+    pub fn move_left(&mut self) {
+        if self.x > 0 {
+            self.x -= 1;
+        }
+    }
+
+    pub fn move_right(&mut self) {
+        if self.x < NUM_COLS - 1 {
+            self.x +=1;
+        }
+    }
+}
