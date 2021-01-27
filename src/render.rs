5fc1b17d46b7c4d2b77753537e19ea77b6b72d3e
thai <maot.tm@gmail.com>
2021-01-27 19:19:35 +0000

    queue up commands

--- src/render.rs
+++ src/render.rs
@@ -1,6 +1,17 @@
 use std::io::Stdout;
 use crate::frame::Frame;
+use crossterm::QueueableCommand;
+use crossterm::style::{ SetBackgroundColor, Color }; 
+use crossterm::terminal::{Clear, ClearType };
 
 pub fn render(stdout: &mut Stdout, last_frame: &Frame, current_frame: &Frame, force: bool) {
+    if force {
+        // clear the entire screen
 
+        // queue up a bunch of commands to the terminal and flush
+        // them all at once
+        stdout.queue(SetBackgroundColor(Color::Blue)).unwrap();
+        stdout.queue(Clear(ClearType::All)).unwrap();
+        stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
+    }
 }
