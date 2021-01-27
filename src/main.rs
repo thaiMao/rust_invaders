c4e9b8375246b70c4e1741a1e6530d16c8f0f298
thai <maot.tm@gmail.com>
2021-01-27 11:52:05 +0000

    enter alternate screen and hide cursor - vice versa on exit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::{ error::Error, io};
+use std::{ io };
+use std::error::Error;
 use rusty_audio::Audio;
-use crossterm::terminal;
+use crossterm::{ terminal, ExecutableCommand };
+use crossterm::terminal::{ EnterAlternateScreen, LeaveAlternateScreen };
+use crossterm::cursor::{ Hide, Show };
 
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>>  {
     let mut audio = Audio::new();
     audio.add("explode", "explode.wav");
     audio.add("lose", "lose.wav");
@@ -17,8 +20,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Terminal
     let mut stdout = io::stdout();
     terminal::enable_raw_mode()?;
+
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
     // Cleanup
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
-
 }
