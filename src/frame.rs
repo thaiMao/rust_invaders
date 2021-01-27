b1fc41c9a4b2dd672cfb1d1895fb01f40953e00a
thai <maot.tm@gmail.com>
2021-01-27 13:35:06 +0000

    add frames populating each col

--- src/frame.rs
+++ src/frame.rs
@@ -1,7 +1,20 @@
 use crate::NUM_COLS;
+use crate::NUM_ROWS;
 
 pub type Frame = Vec<Vec<&'static str>>;
 
 pub fn new_frame() -> Frame {
     let mut cols = Vec::with_capacity(NUM_COLS);
+
+    for _ in 0..NUM_COLS {
+        let mut col = Vec::with_capacity(NUM_ROWS);
+
+        for _ in 0..NUM_ROWS {
+            col.push(" ");
+        }
+
+        cols.push(col);
+    }
+
+    cols
 }
