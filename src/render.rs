a6af3020fdd5c948ee894c85cf23a3059b4c5d61
thai <maot.tm@gmail.com>
2021-01-27 19:31:09 +0000

    iterate over entire fram and draw any change

--- src/render.rs
+++ src/render.rs
@@ -3,6 +3,7 @@ use crate::frame::Frame;
 use crossterm::QueueableCommand;
 use crossterm::style::{ SetBackgroundColor, Color }; 
 use crossterm::terminal::{Clear, ClearType };
+use crossterm::cursor::MoveTo;
 
 pub fn render(stdout: &mut Stdout, last_frame: &Frame, current_frame: &Frame, force: bool) {
     if force {
@@ -14,4 +15,16 @@ pub fn render(stdout: &mut Stdout, last_frame: &Frame, current_frame: &Frame, fo
         stdout.queue(Clear(ClearType::All)).unwrap();
         stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
     }
+
+    // iterate through entire frame and draw what has changed
+    for (x, col) in current_frame.iter().enumerate() {
+        for (y, s) in col.iter().enumerate() {
+            // if the character has changed or we are forcing rendering
+            if *s != last_frame[x][y] || force {
+                stdout.queue(MoveTo(x as u16, y as u16)).unwrap();
+                print!("{}", *s);
+            }
+        }
+    }
+
 }
