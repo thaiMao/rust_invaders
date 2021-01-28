8640ad48c08bbbee1924a81aa29480e316cfd332
thai <maot.tm@gmail.com>
2021-01-28 11:56:50 +0000

    handle when invaders move to far right

--- src/invaders.rs
+++ src/invaders.rs
@@ -46,13 +46,25 @@ impl Invaders {
         if self.move_timer.ready {
             self.move_timer.reset();
 
-            let mut move_downwards = false;
+            let mut downwards = false;
 
             if self.direction == -1 { // if direction is left
 
                 // iter on vec - immutable iterator
                 let min_x = self.army.iter().map(|invader| {invader.x}).min().unwrap_or(0);
 
+                if min_x == 0 {
+                    self.direction = 1;
+                    downwards = true;
+                }
+
+            } else {
+                let max_x = self.army.iter().map(|invader| { invader.x }).max().unwrap_or(0);
+
+                if max_x == NUM_COLS - 1 {
+                    self.direction = -1;
+                    downwards = true;
+                }
             }
             return true;
         }
